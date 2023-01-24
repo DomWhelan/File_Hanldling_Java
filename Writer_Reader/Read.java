d101cbc0ad4740156ba111d97977a6c029ed575f
Dominic Whelan <dominic.whelan27@gmail.com>
2023-01-24 17:44:10 -0330

    Read and Wrrite examples

---
+++ Writer_Reader/Read.java
@@ -0,0 +1,13 @@
+package Writer_Reader;
+
+import java.io.FileReader;
+
+public class Read {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("abc.txt");
+        int i;
+        while ((i=fr.read())!=-1)
+        System.out.println((char)i);
+        fr.close();
+    }
+}
---
+++ Writer_Reader/Write.java
@@ -0,0 +1,17 @@
+package Writer_Reader;
+import java.io.*;
+
+public class Write {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter fw = new FileWriter("abc.txt");
+            fw.write("My name is Dominic");
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Success");
+    }
+    
+}
