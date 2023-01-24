2315a048cb11d48afb485234bc5857c02d9d8850
Dominic Whelan <dominic.whelan27@gmail.com>
2023-01-24 15:14:18 -0330

    first 2 examples, creating files

--- Example_1.java
+++ Example_1.java
@@ -1,6 +1,19 @@
+import java.io.*; 
+
 public class Example_1 {
     
     public static void main(String[] args) {
-        
+
+         try{
+            File file = new File("javaFile1.txt");
+            if(file.createNewFile()){
+                System.out.println("New File Created");
+            } else {
+                System.out.println("File Already Exists");
+            }
+         } catch(IOException e) {
+            e.printStackTrace();
+         }
+
     }
 }
---
+++ Example_2.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class Example_2 {
+    public static void main(String[] args) {
+        String path = "";
+        boolean bool = false;
+        try {
+            File file = new File("javaFile2.txt");
+            file.createNewFile();
+            System.out.println(file);
+            File file2 = file.getCanonicalFile();
+            System.out.println(file2);
+            bool = file2.exists();
+            path = file2.getAbsolutePath();
+            System.out.println(bool);
+            if (bool) {
+                System.out.println(path + " Exists? " + bool);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
